1ae51a862fb8b08e2a252f0fdaf81458c77977b5
sana0803 <sana0803@naver.com>
2022-02-15 18:19:46 +0900

    chore: update README

--- custom-component/src/component/ClickToEdit.js
+++ custom-component/src/component/ClickToEdit.js
@@ -2,8 +2,10 @@ import React, { useState } from 'react';
 import '../css/tab.scss';
 
 const ClickToEdit = () => {
-  const [userName, setUserName] = useState('')
-  const [userAge, setUserAge] = useState('')
+  const [userName, setUserName] = useState('김코딩')
+  const [userAge, setUserAge] = useState('20')
+  const [newName, setNewName] = useState(userName)
+  const [newAge, setNewAge] = useState(userAge)
 
   const nameHandler = (e) => {
     setUserName(e.target.value)
@@ -22,6 +24,9 @@ const ClickToEdit = () => {
             placeholder='이름을 입력하세요.'
             onChange={nameHandler}
             className='user-input'
+            value={userName}
+            onFocus={e => e.target.select()}
+            onBlur={() => setNewName(userName)}
           />
         </div>
         <div className='user-info-wrap'>
@@ -29,12 +34,15 @@ const ClickToEdit = () => {
           <input
             placeholder='나이를 입력하세요.'
             onChange={ageHandler} 
-            className='user-input'       
+            className='user-input'  
+            value={userAge}
+            onFocus={e => e.target.select()}
+            onBlur={() => setNewAge(userAge)}
           />
         </div>
         <div className='user-preview'>
-          <p className='user-name'><b>Name :</b> {userName}</p>
-          <p><b>Age :</b> {userAge}</p>
+          <p className='user-name'><b>Name :</b> {newName}</p>
+          <p><b>Age :</b> {newAge}</p>
         </div>    
       </div>
     </div>
